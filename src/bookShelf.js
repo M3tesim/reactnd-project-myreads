89c79f5bd11472e4696b278fbaf97d301ecc1a49
Moatasem <m3tsmsh@gmail.com>
2021-11-22 17:35:25 +0200

    we have daynamically add the books in our array to the shelfs

--- src/bookShelf.js
+++ src/bookShelf.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import BookList from './bookList'
+import * as BooksAPI from './BooksAPI'
 
 class BookShelf extends Component {
 
@@ -9,22 +10,34 @@ class BookShelf extends Component {
 
   }
 
+   //this function shoul add all the books in the shelf to the stat array "books"
+
+   Booklist = async ()=>{
+  const books = await BooksAPI.getAll()
+  this.setState(()=> (
+    { books : books
+
+  }))
+};
+
+componentDidMount = () => {
+  this.Booklist();
+}
 
     render(){
 
-       const {currentState}= this.props
-
+       const {shelf }= this.props;
+       console.log(this.state.books);
         return(
 
-        
                 <div className="bookshelf">
-                  <h2 className="bookshelf-title">{currentState}</h2>
+                  <h2 className="bookshelf-title">{shelf}</h2>
                   <BookList books={this.state.books}  />
 
 
                 </div>
         )
-    }
+    } 
 }
 
 
